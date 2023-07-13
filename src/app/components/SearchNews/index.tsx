bcc44afa94e4b703a20af96efc93e6fe3d8a9fa8
ZZZdream <kmultan1234@gmail.com>
2023-07-13 15:54:17 +0200

    Added styled searching form for searching news

---
+++ src/app/components/SearchNews/index.tsx
@@ -0,0 +1,20 @@
+import { Dispatch, SetStateAction } from "react";
+import "./styles.scss";
+
+const SearchNews = ({setSearchQuery}:any) => {
+    function submitForm(e:any){
+        e.preventDefault();
+        const form = document.forms[0];
+        const input = form.elements[0] as HTMLInputElement;
+        setSearchQuery(input.value);
+        input.value = "";
+    }
+    return (
+        <form className="search" onSubmit={(e) => submitForm(e)}>
+            <input type="text" className="search-input" placeholder="Search new things"/>
+            <button type="submit" className="search-button">Search</button>
+        </form>
+    )
+}
+
+export default SearchNews;
