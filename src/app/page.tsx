64291e54dc143dfa8f5f40b2f74552856b290873
ZZZdream <kmultan1234@gmail.com>
2023-07-13 15:56:08 +0200

    implemented Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,92 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+import { QueryClient, QueryClientProvider } from "react-query";
+import News from "./components/News";
+import styles from "./page.module.scss";
+import { useState } from "react";
+import SearchNews from "./components/SearchNews";
+
+import Image from "next/image";
 
 export default function Home() {
+  const queryClient = new QueryClient();
+  const [searchQuery, setSearchQuery] = useState("javascript");
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
+    <QueryClientProvider client={queryClient}>
+      <main className={styles.main}>
+        <div className={styles.description}>
+          <h1 className={styles.title}>HOT NEWS</h1>
+          <h2 className={styles.subtitle}>
+            Find spicest news about anything you want
+          </h2>
+          <SearchNews setSearchQuery={setSearchQuery} />
+          <h2 className={styles.search}>Your search:</h2>
+          <h2 className={styles.searchResult}>{searchQuery.toUpperCase()}</h2>
+          <div className={styles.blob}>
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
+              src={"blobanimation.svg"}
+              alt={"news image"}
+              width={30}
+              height={30}
+              layout="responsive"
+              loading="eager"
               priority
             />
-          </a>
+          </div>
         </div>
-      </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <div className={styles.center}>
+          <News searchQuery={searchQuery} />
+        </div>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+        <div className={styles.grid}>
+          <a
+            href=""
+            className={styles.card}
+            onClick={(e) => {
+              e.preventDefault();
+              window.open("", "_blank");
+            }}
+          >
+            <h3>Documentation &rarr;</h3>
+            <p>Find in-depth information about Next.js features and API.</p>
+          </a>
+          <a
+            href=""
+            className={styles.card}
+            onClick={(e) => {
+              e.preventDefault();
+              window.open("", "_blank");
+            }}
+          >
+            <h3>Documentation &rarr;</h3>
+            <p>Find in-depth information about Next.js features and API.</p>
+          </a>
+          <a
+            href=""
+            className={styles.card}
+            onClick={(e) => {
+              e.preventDefault();
+              window.open("", "_blank");
+            }}
+          >
+            <h3>Documentation &rarr;</h3>
+            <p>Find in-depth information about Next.js features and API.</p>
+          </a>
+          <a
+            href=""
+            className={styles.card}
+            onClick={(e) => {
+              e.preventDefault();
+              window.open("", "_blank");
+            }}
+          >
+            <h3>Documentation &rarr;</h3>
+            <p>Find in-depth information about Next.js features and API.</p>
+          </a>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+        </div>
+      </main>
+    </QueryClientProvider>
+  );
 }
